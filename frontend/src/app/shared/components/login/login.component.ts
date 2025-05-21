71b7bc44303a44e8e1acd6657dc7e6558dc8ea3e
garnicamatias <garnicamatias02@gmail.com>
2025-05-21 01:42:24 -0300

    Diseño y Estructura de Login OK, sin funcionalidad

--- frontend/src/app/shared/components/login/login.component.ts
+++ frontend/src/app/shared/components/login/login.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import {MatCard} from '@angular/material/card'
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import {BotonPrimarioComponent} from '../boton-primario/boton-primario.component'
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [MatFormFieldModule, MatInputModule, MatSelectModule, MatButtonModule, MatIconModule, MatCard, BotonPrimarioComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
 }
+
+
+// clickEvent(event: MouseEvent) {
+//   this.hide = !this.hide;
+//   event.stopPropagation();
+// }
--- frontend/src/app/shared/components/toolbar/toolbar.component.ts
+++ frontend/src/app/shared/components/toolbar/toolbar.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
 @Component({
   selector: 'app-toolbar',
-  imports: [],
+  standalone: true,
+  imports: [MatToolbarModule],
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.css'
 })
