7e945430543cdbd93bcc2d456ef47ce7b37c5287
CynthiaCG <cynthiaestef@gmail.com>
2025-06-19 22:01:20 -0300

    fix import

--- backend/src/modules/cliente/cliente.controller.ts
+++ backend/src/modules/cliente/cliente.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, HttpStatus } from '@nestjs/common';
 import { ClienteService } from './cliente.service';
 import { CreateClienteDto } from './dto/create-cliente.dto';
 import { UpdateClienteDto } from './dto/update-cliente.dto';
